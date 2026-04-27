766591905302b49cc83fa908cba6806fa860b82d
bilalshezad <bilalshezad000@gmail.com>
2026-04-26 21:13:03 -0700

    asdf

--- src/components/Jobs/AddJob.jsx
+++ src/components/Jobs/AddJob.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { handleLoading, } from '../../utils';
-import { toast, ToastContainer} from 'react-toastify'
+import { toast} from 'react-toastify'
 
 
 export default function AddJob({ reload }) {
--- src/components/Jobs/JobList.jsx
+++ src/components/Jobs/JobList.jsx
@@ -1,5 +1,5 @@
 import React, {  useState , useEffect} from "react";
-
+  
 export default function JobList({ reload }) {
   const [jobs, setJobs] = useState([]);
   const [editId, setEditId] = useState(null);
